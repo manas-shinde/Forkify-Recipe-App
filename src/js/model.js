aa2c56ffae2fafc5c5bc4b53531452b921cd782f
manas-shinde <manas.a.shinde11@gmail.com>
2023-05-07 10:40:35 +0530

    Added 'loadSearchRecipes' method to retrive list of recipes basics on query.

--- src/js/model.js
+++ src/js/model.js
@@ -4,11 +4,15 @@ import { getJson } from './helpers';
 
 export const state = {
   recipe: {},
+  serach: {
+    query: '',
+    result: [],
+  },
 };
 
 export const loadRecipe = async recipeId => {
   try {
-    const data = await getJson(`${API_URL}/${recipeId}`);
+    const data = await getJson(`${API_URL}${recipeId}`);
 
     // Extract recipe object for response
     let { recipe } = data.data;
@@ -25,6 +29,24 @@ export const loadRecipe = async recipeId => {
     };
 
     console.log(state.recipe);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const loadSearchRecipes = async query => {
+  try {
+    const data = await getJson(`${API_URL}?search=${query}`);
+
+    state.serach.result = data.data.recipes.map(recipe => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url,
+      };
+    });
   } catch (error) {
     console.error(error);
     throw error;
