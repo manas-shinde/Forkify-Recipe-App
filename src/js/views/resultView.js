871d3834c01b65de09a3120ef8d23959b3eb45cc
manas-shinde <manas.a.shinde11@gmail.com>
2023-05-07 10:38:58 +0530

    class to render list of recipes.

---
+++ src/js/views/resultView.js
@@ -0,0 +1,29 @@
+import View from './View';
+import icons from '../../img/icons.svg';
+
+class ResultView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = `No recipes found for your query. Please try again!`;
+
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data.map(this._generateMarkupPreview).join('');
+  }
+  _generateMarkupPreview(recipe) {
+    return `
+    <li class="preview">
+        <a class="preview__link" href="#${recipe.id}">
+              <figure class="preview__fig">
+                <img src="${recipe.image}" alt="${recipe.title}" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${recipe.title}</h4>
+                <p class="preview__publisher">${recipe.publisher}</p>
+              </div>
+        </a>
+    </li>
+    `;
+  }
+}
+
+export default new ResultView();
