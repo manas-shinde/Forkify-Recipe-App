1c1beec67a75083baf918675733c8c2f135b6990
manas-shinde <manas.a.shinde11@gmail.com>
2023-05-21 00:35:14 +0530

    FEAT : Implemented PreviewView child class to make _generateMarkup more Generic.

--- src/js/views/resultView.js
+++ src/js/views/resultView.js
@@ -1,4 +1,5 @@
 import View from './View';
+import previewView from './previewView';
 import icons from '../../img/icons.svg';
 
 class ResultView extends View {
@@ -6,26 +7,7 @@ class ResultView extends View {
   _errorMessage = `No recipes found for your query. Please try again!`;
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-  _generateMarkupPreview(recipe) {
-    const id = window.location.hash.slice(1);
-
-    return `
-    <li class="preview">
-        <a class="preview__link ${
-          recipe.id == id ? 'preview__link--active' : ''
-        }" href="#${recipe.id}">
-              <figure class="preview__fig">
-                <img src="${recipe.image}" alt="${recipe.title}" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${recipe.title}</h4>
-                <p class="preview__publisher">${recipe.publisher}</p>
-              </div>
-        </a>
-    </li>
-    `;
+    return this._data.map(result => previewView.render(result, false)).join('');
   }
 }
 
