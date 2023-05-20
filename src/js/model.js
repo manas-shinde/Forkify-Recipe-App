95b95f17938da1456fcbc8f3fae93d0b907427e1
manas-shinde <manas.a.shinde11@gmail.com>
2023-05-21 01:13:12 +0530

    FEAT : Store the bookmarks array in local storage.

--- src/js/model.js
+++ src/js/model.js
@@ -77,12 +77,18 @@ export const getSearchResultPage = (page = state.currentPage) => {
   return state.search.result.slice(start, end);
 };
 
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
 export const addBookmark = recipe => {
   // Add bookmark
   state.bookmarks.push(recipe);
 
   // Mark current recipe as bookmarked
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+  persistBookmarks();
 };
 
 export const removeBookmark = id => {
@@ -92,4 +98,12 @@ export const removeBookmark = id => {
 
   // remove current recipe bookmark
   if (id === state.recipe.id) state.recipe.bookmarked = false;
+
+  persistBookmarks();
 };
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+init();
